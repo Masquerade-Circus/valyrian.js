3f452515938c778db4fb6bd38013686220f4b99b
Masquerade Circus <christian@masquerade-circus.net>
2019-11-22 16:47:20 -0600

    Improve performance

--- test/hooks_test.js
+++ test/hooks_test.js
@@ -2,24 +2,27 @@ import expect from 'expect';
 import '../lib';
 import nodePlugin from '../plugins/node';
 v.usePlugin(nodePlugin);
-v.useHooks();
 
-describe.only('Hooks like pattern', () => {
+describe('Hooks like pattern', () => {
 
   it('should create a simple counter', async () => {
     v.unmount();
     let Counter = () => {
       let [count, setState] = v.useState(0);
-      let interval = setInterval(() => setState(count + 1), 1000);
-      v.useCleanup(() => clearInterval(interval));
-      return <div>{count}</div>;
+      let [ok, setOk] = v.useState('ok');
+      let interval = setInterval(() => {
+        setState(count + 1);
+        setOk('notOk');
+      }, 1000);
+      v.onCleanup(() => clearInterval(interval));
+      return <div>{count} {ok}</div>;
     };
 
     let result = v.mount('div', Counter);
-    expect(result).toEqual('<div>0</div>');
+    expect(result).toEqual('<div>0 ok</div>');
     await new Promise(resolve => setTimeout(() => resolve(), 2050));
     result = v.update();
-    expect(result).toEqual('<div>2</div>');
+    expect(result).toEqual('<div>2 notOk</div>');
     result = v.unmount();
   });
 });
