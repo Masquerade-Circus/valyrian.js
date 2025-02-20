909c99afce4fc4574fb571ae3142c7c996cbcd3c
Christian Robledo <christian@masquerade-circus.net>
2025-02-19 18:13:25 -0600

    refactor: update service worker example

--- lib/node/utils/sw.ts
+++ lib/node/utils/sw.ts
@@ -28,27 +28,35 @@ export function sw(file: string, options = {}) {
 }
 
 /*
+  // On the server side to generate the service worker
   sw("sw.js", {
     version: "1",
     name: "Valyrian.js",
-    urls: ["/", "/index.html"],
-    debug: false
+    urls: ["/"],
+    debug: false,
+    logFetch: false,
+    offlinePage: "/offline.html"
   });
 
 
   // On the client side
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js");
-
-    navigator.serviceWorker.addEventListener("message", (event) => {
-      if (event.data && event.data.type === "NEW_VERSION") {
-        // Notify the user about the new version and ask if they want to update
-        if (confirm("Hay una nueva versión disponible. ¿Deseas actualizar?")) {
-          // Send a message to the service worker to skip the waiting
-          navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
-          window.location.reload();
+    const registration = await navigator.serviceWorker.register("/sw.js");
+    registration.addEventListener("updatefound", () => {
+      const newWorker = registration.installing as ServiceWorker;
+      newWorker.addEventListener("statechange", () => {
+        if (newWorker.state === "installed") {
+          // Notify the user about the new version and ask if they want to update
+          if (confirm("There is a new version available. Do you want to update?")) {
+            // Send a message to the service worker to skip the waiting
+            navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
+          }
         }
-      }
+      });
+    });
+
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      window.location.reload();
     });
   }
 */
